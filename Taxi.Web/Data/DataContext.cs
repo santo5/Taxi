9a6835ab3b614d9f715a19341cf805fc18184522
USUARIO <USUARIO@DESKTOP-KQ4THBV>
2020-03-30 18:09:12 -0500

    Group Usuario BD

--- Taxi.Web/Data/DataContext.cs
+++ Taxi.Web/Data/DataContext.cs
@@ -14,6 +14,9 @@ namespace Taxi.Web.Data
         public DbSet<TripEntity> Trips { get; set; }
         public DbSet<TripDetailEntity> TripDetails { get; set; }
         public DbSet<UserGroupEntity> UserGroups { get; set; }
+        public DbSet<UserGroupDetailEntity> UserGroupDetails { get; set; }
+        public DbSet<UserGroupRequestEntity> UserGroupRequests { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
