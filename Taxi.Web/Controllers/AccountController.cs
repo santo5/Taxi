40278191877df5abd0d9a0ca121a59b7aa402a8b
USUARIO <USUARIO@DESKTOP-KQ4THBV>
2020-03-30 19:12:18 -0500

    Asignar un usuario a un grupo

--- Taxi.Web/Controllers/AccountController.cs
+++ Taxi.Web/Controllers/AccountController.cs
@@ -10,6 +10,10 @@ using System.IdentityModel.Tokens.Jwt;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Text;
+using Taxi.Web.Data;
+using Microsoft.EntityFrameworkCore;
+using Taxi.Common.Enums;
+using System.Collections.Generic;
 
 namespace Taxi.Web.Controllers
 {
@@ -21,12 +25,14 @@ namespace Taxi.Web.Controllers
         private readonly ICombosHelper _combosHelper;
         private readonly IConfiguration _configuration;
         private readonly IMailHelper _mailHelper;
+        private readonly DataContext _context;
 
         public AccountController(IUserHelper userHelper,
             IImageHelper imageHelper,
             ICombosHelper combosHelper,
             IConfiguration configuration,
-            IMailHelper mailHelper
+            IMailHelper mailHelper,
+            DataContext context
 )
         {
             _userHelper = userHelper;
@@ -34,6 +40,82 @@ namespace Taxi.Web.Controllers
             _combosHelper = combosHelper;
             _configuration = configuration;
             _mailHelper = mailHelper;
+            _context = context;
+        }
+        public async Task<IActionResult> ConfirmUserGroup(int requestId, string token)
+        {
+            if (requestId == 0 || string.IsNullOrEmpty(token))
+            {
+                return NotFound();
+            }
+
+            UserGroupRequestEntity userGroupRequest = await _context.UserGroupRequests
+                .Include(ugr => ugr.ProposalUser)
+                .Include(ugr => ugr.RequiredUser)
+                .FirstOrDefaultAsync(ugr => ugr.Id == requestId &&
+                                            ugr.Token == new Guid(token));
+            if (userGroupRequest == null)
+            {
+                return NotFound();
+            }
+
+            await AddGroupAsync(userGroupRequest.ProposalUser, userGroupRequest.RequiredUser);
+            await AddGroupAsync(userGroupRequest.RequiredUser, userGroupRequest.ProposalUser);
+
+            userGroupRequest.Status = UserGroupStatus.Accepted;
+            _context.UserGroupRequests.Update(userGroupRequest);
+            await _context.SaveChangesAsync();
+            return View();
+        }
+
+        private async Task AddGroupAsync(UserEntity proposalUser, UserEntity requiredUser)
+        {
+            UserGroupEntity userGroup = await _context.UserGroups
+                .Include(ug => ug.Users)
+                .ThenInclude(u => u.User)
+                .FirstOrDefaultAsync(ug => ug.User.Id == proposalUser.Id);
+            if (userGroup != null)
+            {
+                UserGroupDetailEntity user = userGroup.Users.FirstOrDefault(u => u.User.Id == requiredUser.Id);
+                if (user == null)
+                {
+                    userGroup.Users.Add(new UserGroupDetailEntity { User = requiredUser });
+                }
+
+                _context.UserGroups.Update(userGroup);
+            }
+            else
+            {
+                _context.UserGroups.Add(new UserGroupEntity
+                {
+                    User = proposalUser,
+                    Users = new List<UserGroupDetailEntity>
+            {
+                new UserGroupDetailEntity { User = requiredUser }
+            }
+                });
+            }
+        }
+
+        public async Task<IActionResult> RejectUserGroup(int requestId, string token)
+        {
+            if (requestId == 0 || string.IsNullOrEmpty(token))
+            {
+                return NotFound();
+            }
+
+            UserGroupRequestEntity userGroupRequest = await _context
+                .UserGroupRequests.FirstOrDefaultAsync(ugr => ugr.Id == requestId &&
+                                                        ugr.Token == new Guid(token));
+            if (userGroupRequest == null)
+            {
+                return NotFound();
+            }
+
+            userGroupRequest.Status = UserGroupStatus.Rejected;
+            _context.UserGroupRequests.Update(userGroupRequest);
+            await _context.SaveChangesAsync();
+            return View();
         }
 
         [HttpPost]
