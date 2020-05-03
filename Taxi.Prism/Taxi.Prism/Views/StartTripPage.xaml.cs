da218a237abc0ab09f0ef72934617950e164db5a
USUARIO <USUARIO@DESKTOP-KQ4THBV>
2020-05-02 20:53:46 -0500

    Implementacion slider

--- Taxi.Prism/Taxi.Prism/Views/StartTripPage.xaml.cs
+++ Taxi.Prism/Taxi.Prism/Views/StartTripPage.xaml.cs
@@ -11,12 +11,21 @@ namespace Taxi.Prism.Views
     {
         private readonly IGeolocatorService _geolocatorService;
         private static StartTripPage _instance;
+        private double _distance;
+        private Position _position;
 
         public StartTripPage(IGeolocatorService geolocatorService)
         {
             InitializeComponent();
             _geolocatorService = geolocatorService;
             _instance = this;
+            _distance = .2;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            MoveMapToCurrentPositionAsync();
         }
 
         public static StartTripPage GetInstance()
@@ -26,6 +35,7 @@ namespace Taxi.Prism.Views
 
         public void AddPin(Position position, string address, string label, PinType pinType)
         {
+            _position = position;
             MyMap.Pins.Add(new Pin
             {
                 Address = address,
@@ -35,37 +45,6 @@ namespace Taxi.Prism.Views
             });
         }
 
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-            MoveMapToCurrentPositionAsync();
-        }
-
-        private async void MoveMapToCurrentPositionAsync()
-        {
-            bool isLocationPermision = await CheckLocationPermisionsAsync();
-
-            if (isLocationPermision)
-            {
-                MyMap.IsShowingUser = true;
-
-                await _geolocatorService.GetLocationAsync();
-                if (_geolocatorService.Latitude != 0 && _geolocatorService.Longitude != 0)
-                {
-                    Position position = new Position(
-                        _geolocatorService.Latitude,
-                        _geolocatorService.Longitude);
-                    MoveMap(position);
-                }
-            }
-        }
-
-        private void MoveMap(Position position)
-        {
-            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(
-                position,
-                Distance.FromKilometers(.2)));
-        }
         public void DrawLine(Position a, Position b)
         {
             if (Device.RuntimePlatform == Device.Android)
@@ -85,7 +64,32 @@ namespace Taxi.Prism.Views
                 AddPin(b, string.Empty, string.Empty, PinType.SavedPin);
             }
 
-            MoveMap(b);
+            _position = b;
+            MoveMap();
+        }
+
+        private async void MoveMapToCurrentPositionAsync()
+        {
+            bool isLocationPermision = await CheckLocationPermisionsAsync();
+
+            if (isLocationPermision)
+            {
+                MyMap.IsShowingUser = true;
+
+                await _geolocatorService.GetLocationAsync();
+                if (_geolocatorService.Latitude != 0 && _geolocatorService.Longitude != 0)
+                {
+                    _position = new Position(
+                        _geolocatorService.Latitude,
+                        _geolocatorService.Longitude);
+                    MoveMap();
+                }
+            }
+        }
+
+        private void MoveMap()
+        {
+            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(_position, Distance.FromKilometers(_distance)));
         }
 
         private async Task<bool> CheckLocationPermisionsAsync()
@@ -109,6 +113,12 @@ namespace Taxi.Prism.Views
             return permissionLocation == PermissionStatus.Granted ||
                    permissionLocationAlways == PermissionStatus.Granted ||
                    permissionLocationWhenInUse == PermissionStatus.Granted;
+        }
+
+        private void MySlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            _distance = e.NewValue;
+            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(_position, Distance.FromKilometers(_distance)));
         }
     }
 }
